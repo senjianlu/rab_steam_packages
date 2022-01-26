7a52713e92a98f159185886a25db77822d26e792
Rabbir <m18018230731@163.com>
2022-01-26 13:32:38 +0800

    feat: 新增 __init__.py 包初始化模块

---
+++ __init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:UTF-8 -*-
+#
+# @AUTHOR: Rabbir
+# @FILE: /root/GitHub/rab_steam_packages/__init__.py
+# @DATE: 2022/01/26 Wed
+# @TIME: 13:31:48
+#
+# @DESCRIPTION: 包初始化
+
+
+import sys
+sys.path.append("../rab_steam_packages") if "../steam_steam_packages" not in sys.path else True
